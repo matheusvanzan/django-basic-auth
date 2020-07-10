bedc380a02f515acf964c9a64937a23f1c9de54b
Matheus Vanzan <vanzan2015@gmail.com>
2020-07-10 16:21:40 +0000

    fix minor

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13%@=qi!pe%re_i1)d9#a31-b1^-a+-&ee4be*xce++4+t28$u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
